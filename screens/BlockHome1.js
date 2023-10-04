9d460b2a57bea472492fa644e89a16c74436c7e0
thuyhapnv24b <vothuyha2003@gmail.com>
2023-10-04 15:03:40 +0700

    add folder screens

---
+++ screens/BlockHome1.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BlockHome1 = () => {
+  return (
+    <View>
+      <Text>BlockHome11</Text>
+    </View>
+  )
+}
+
+export default BlockHome1

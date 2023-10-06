0370cb02ed514986b58fc31ae09124eafc0bf492
trungpnv24b <dangtrungps@gmail.com>
2023-10-06 16:44:51 +0700

    up

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from "react"
-import { NavigationContainer } from "@react-navigation/native"
-import Tabs from "./navigation/tabs"
-const App = () => {
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BlockHome1 from './screens/BlockHome1';
+import BlockHome2 from './screens/BlockHome2';
+import BlockHome3 from './screens/BlockHome3';
+
+export default function App() {
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
-  )
+    <>
+      {/* <BlockHome1></BlockHome1> */}
+      {/* <BlockHome2></BlockHome2> */}
+      <BlockHome3></BlockHome3>
+    </>
+  );
 }
-export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

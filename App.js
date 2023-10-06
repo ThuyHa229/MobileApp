8badada666da84c4e806852a4424ad0e4037973b
thuyhapnv24b <vothuyha2003@gmail.com>
2023-10-06 15:31:09 +0700

    hafinish

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import BlockHome1 from './screens/BlockHome1';
+import BlockHome2 from './screens/BlockHome2';
+import BlockHome3 from './screens/BlockHome3';
 
 export default function App() {
   return (
    <>
-    <BlockHome1></BlockHome1>
+    {/* <BlockHome1></BlockHome1> */}
+    {/* <BlockHome2></BlockHome2> */}
+    <BlockHome3></BlockHome3>
    </>
   );
 }
--- screens/BlockHome1.js
+++ screens/BlockHome1.js
@@ -1,12 +1,233 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 const BlockHome1 = () => {
   return (
-    <View>
-      <Text>BlockHome11</Text>
-    </View>
-  )
-}
+    <>
+      <View style={styles.HomeBody}>
+        <View style={styles.bodyhome}>
+          <View style={styles.container}>
+            <Image
+              source={require("../assets/pattern.png")}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+            <Image
+              source={require("../assets/imagekem.png")}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 15,
+                height: "100%",
+              }}
+            />
+            <View>
+              <Text style={styles.text1}>Special Deal For October</Text>
+            </View>
+            <View>
+              <TouchableOpacity
+                style={styles.buttonContainer}
+                onPress={() => {}}
+              >
+                <Text style={styles.buttonText}>Buy Now</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.nearestRestaurant}>
+            <View>
+              <Text style={styles.restaurantName}>Nearest Restaurant</Text>
+            </View>
+            <View>
+              <Text style={styles.viewmore}>View More</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.restaurant}>
+          <View style={styles.rest1}>
+            <Image source={require("../assets/restaurantImage2.png")} />
+            <Text style={styles.restName1}>Vegan Resto</Text>
+            <Text style={styles.restTime1}>12 Mins</Text>
+          </View>
+          <View style={styles.rest2}>
+            <Image source={require("../assets/restaurantImage1.png")} />
+            <Text style={styles.restName2}>Healthy Food</Text>
+            <Text style={styles.restTime2}>8 Mins</Text>
+          </View>
+        </View>
+        <View style={styles.popular}>
+          <View>
+            <Text style={styles.popularMenu}>Popular Menu</Text>
+          </View>
+          <View>
+            <Text style={styles.viewmore1}>View More</Text>
+          </View>
+        </View>
+        <View>
+          <View style={styles.dish}>
+            <Image
+              source={require("../assets/photomenu.png")}
+              style={{
+                width: 100,
+                height: 100,
+              }}
+            />
+            <Text style={styles.namedish}>Green Noodles</Text>
+            <View>
+              <Text style={styles.descri}>Noodles Home</Text>
+            </View>
+            <Text style={styles.price}>$15</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
 
-export default BlockHome1
+export default BlockHome1;
+
+const styles = StyleSheet.create({
+  HomeBody: {
+    backgroundColor: "#f0edfe",
+    height: "100%",
+  },
+  bodyhome: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#6B50F6",
+    height: 170,
+    borderRadius: 20,
+    width: 400,
+    marginTop: 200,
+  },
+  text1: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 170,
+    marginTop: -140,
+  },
+  buttonContainer: {
+    backgroundColor: "white",
+    paddingVertical: 8,
+    paddingHorizontal: 22,
+    borderRadius: 8,
+    marginTop: -65,
+    width: 100,
+    height: 35,
+    marginLeft: 80,
+  },
+  buttonText: {
+    color: "#6B50F6",
+    fontWeight: "bold",
+    fontSize: 13,
+  },
+  nearestRestaurant: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  restaurantName: {
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  viewmore: {
+    color: "#6B50F6",
+    marginTop: 0,
+    marginLeft: 170,
+    fontSize: 15,
+  },
+  restaurant: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rest1: {
+    marginTop: 20,
+    backgroundColor: "white",
+    marginLeft: 30,
+    borderRadius: 20,
+    width: 180,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  restName1: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  restTime1: {
+    fontSize: 15,
+    color: "gray",
+    marginTop: 5,
+  },
+  rest2: {
+    marginTop: 20,
+    backgroundColor: "white",
+    marginRight: 30,
+    borderRadius: 20,
+    width: 180,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  restName2: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  restTime2: {
+    fontSize: 15,
+    color: "gray",
+    marginTop: 5,
+  },
+  popular: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  popularMenu: {
+    marginLeft: 40,
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  viewmore1: {
+    color: "#6B50F6",
+    marginTop: 0,
+    fontSize: 15,
+    marginRight: 40,
+  },
+  dish: {
+    marginTop: 30,
+    backgroundColor: "white",
+    marginLeft: 30,
+    borderRadius: 20,
+    width: "90%",
+    height: "auto",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  namedish: {
+    marginTop: 20,
+    marginRight: 120,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  descri: {
+    marginTop: 50,
+    marginLeft: -248,
+    color: 'gray'
+  },
+  price: {
+    marginTop: 20,
+    marginRight: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#6B50F6",
+  },
+});
